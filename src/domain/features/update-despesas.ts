a2365724b7e2b2b6315faa2c6f83486310115f80
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-04 12:45:24 -0300

    refactor: Update update-despesas feature to use Despesa entity and repository

--- src/domain/features/update-despesas.ts
+++ src/domain/features/update-despesas.ts
@@ -1,42 +1,42 @@
-import { Despesas } from "@/domain/entities/despesa";
-import { IDespesasRepository } from "@/domain/contracts/despesas-repository";
+import { Despesa } from '@/domain/entities/despesa';
+import { IDespesasRepository } from '@/domain/contracts/despesas-repository';
 
 type Input = {
-	id: string;
-	name: string;
-	description: string;
-	value: number;
-	date: Date;
+  id: string;
+  name: string;
+  description: string;
+  value: number;
+  date: Date;
 };
-type OutPut = Despesas;
-type UpdateDespesas = (input: Input) => Promise<OutPut>;
+type OutPut = Despesa;
+type UpdateDespesa = (input: Input) => Promise<OutPut>;
 type SetupUpdateBoard = {
-	repository: IDespesasRepository;
+  repository: IDespesasRepository;
 };
-type Setup = (props: SetupUpdateBoard) => UpdateDespesas;
+type Setup = (props: SetupUpdateBoard) => UpdateDespesa;
 
 export const setupUpdateDespesas: Setup =
-	({ repository }) =>
-	async ({ id, description, value, date, name }: Input) => {
-		const despesa = await repository.getDespesaById(id);
-		if (!despesa)
-			throw new Error(`Despesas with the id ${id} not found`, {
-				cause: "despesas-not-found",
-			});
-		if (!description) throw new Error("Descrição é obrigatória");
-		if (!value) throw new Error("value é obrigatório");
-		if (!name) throw new Error("Nome é obrigatório");
-		try {
-			return await repository.updateDespesa(id, {
-				name,
-				description,
-				value,
-				date,
-				deleted: false,
-			});
-		} catch (error) {
-			throw new Error("Could not update despesa: " + error, {
-				cause: "update-despesas",
-			});
-		}
-	};
+  ({ repository }) =>
+  async ({ id, description, value, date, name }: Input) => {
+    const despesa = await repository.getDespesaById(id);
+    if (!despesa)
+      throw new Error(`Despesawith the id ${id} not found`, {
+        cause: 'despesas-not-found',
+      });
+    if (!description) throw new Error('Descrição é obrigatória');
+    if (!value) throw new Error('value é obrigatório');
+    if (!name) throw new Error('Nome é obrigatório');
+    try {
+      return await repository.updateDespesa(id, {
+        name,
+        description,
+        value,
+        date,
+        deleted: false,
+      });
+    } catch (error) {
+      throw new Error('Could not update despesa: ' + error, {
+        cause: 'update-despesas',
+      });
+    }
+  };
