2c9b7b06033f286015df5c513d56e452c07f7b55
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-04 12:35:39 -0300

    feat: Add UserEntity, ReceitasEntity, and DespesasEntity to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { UserEntity } from './infra/database/entities/user.entity';
+import { ReceitasEntity } from './infra/database/entities/receitas.entity';
+import { DespesasEntity } from './infra/database/entities/despesas.entity';
 
 @Module({
   imports: [
@@ -11,10 +13,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'test',
       password: 'test',
       database: 'test',
-      entities: [User],
+      entities: [UserEntity, ReceitasEntity, DespesasEntity],
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([UserEntity, ReceitasEntity, DespesasEntity]),
   ],
 })
 export class AppModule {}
