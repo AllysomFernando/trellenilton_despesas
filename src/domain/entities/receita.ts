943b1d6f703d87c096ee57e5dd63a62d34f8c106
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-04 12:49:19 -0300

    feat: Update Receita entity with optional id field

--- src/domain/entities/receita.ts
+++ src/domain/entities/receita.ts
@@ -1,10 +1,10 @@
 export class Receita {
   constructor(
-    public readonly id: string,
     public readonly name: string,
     public readonly description: string,
     public readonly value: number,
     public readonly date: Date,
     public readonly deleted: boolean,
+    public readonly id?: string,
   ) {}
 }
