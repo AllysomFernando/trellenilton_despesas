d9a10548e6eaa57535bd356e9727fc1e9f729a5b
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-04 14:29:12 -0300

    refactor: Implement DespesasController with DespesasService

---
+++ src/modules/despesas/despesas.controller.ts
@@ -0,0 +1,39 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { DespesasService } from './despesas.services';
+
+export class DespesasController {
+  constructor(private readonly despesasService: DespesasService) {}
+
+  @Get()
+  async findAll() {
+    return await this.despesasService.findAll();
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return await this.despesasService.findById(id);
+  }
+
+  @Post()
+  async create(@Body() despesa) {
+    return await this.despesasService.create(despesa);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() despesa) {
+    return await this.despesasService.update(id, despesa);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return await this.despesasService.delete(id);
+  }
+}
