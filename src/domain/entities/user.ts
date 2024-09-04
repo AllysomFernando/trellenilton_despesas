5750f547b52c882e1bee15c241ee7f99bb7ff5f5
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-04 12:36:55 -0300

    refactor: Update User entity to use class syntax

--- src/domain/entities/user.ts
+++ src/domain/entities/user.ts
@@ -1,7 +1,9 @@
-export type User = {
-  id?: string;
-  name: string;
-  email: string;
-  password: string;
-  deleted: boolean;
-};
+export class User {
+  constructor(
+    public readonly id: string,
+    public readonly name: string,
+    public readonly email: string,
+    public readonly password: string,
+    public readonly deleted: boolean,
+  ) {}
+}
