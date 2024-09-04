b6c21aeacd13852ef0081b94d452b17ca616a1c0
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-04 14:59:13 -0300

    feat: Adicionar controlador de usuário (Add user controller)

---
+++ src/infra/modules/user/user.controller.ts
@@ -0,0 +1,39 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { UserService } from './user.services';
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async findAll() {
+    return await this.userService.findAll();
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return await this.userService.findById(id);
+  }
+
+  @Post()
+  async create(@Body() user) {
+    return await this.userService.create(user);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() user) {
+    return await this.userService.update(id, user);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return await this.userService.delete(id);
+  }
+}
