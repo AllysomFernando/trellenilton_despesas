fb2b92b3ebddcd1f148fdba40cbae3ee5f0dfcdc
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-04 14:46:35 -0300

    feat: Adicionar serviço de receitas (Add receitas service)

---
+++ src/infra/modules/receitas/receitas.services.ts
@@ -0,0 +1,25 @@
+import { ReceitasRepository } from '@/infra/repositories/receitas-repository';
+
+export class ReceitasServices {
+  constructor(private readonly db: ReceitasRepository) {}
+
+  async findAll() {
+    return await this.db.getReceitas();
+  }
+
+  async findById(id: string) {
+    return await this.db.getReceitaById(id);
+  }
+
+  async create(receita) {
+    return await this.db.createReceita(receita);
+  }
+
+  async update(id, receita) {
+    return await this.db.updateReceita(id, receita);
+  }
+
+  async delete(id) {
+    return await this.db.deleteReceita(id);
+  }
+}
