612cd0d6084d35a1c5cf5040de471219e4c07d61
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-04 15:28:08 -0300

    feat: Adicionar importação do TypeOrmModule para entidade de usuário (Add TypeOrmModule import for user entity)

--- src/infra/modules/user/user.modules.ts
+++ src/infra/modules/user/user.modules.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.services';
 import { UserRepository } from '@/infra/repositories/user-repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '@/infra/database/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
   providers: [UserService, UserRepository],
 })
