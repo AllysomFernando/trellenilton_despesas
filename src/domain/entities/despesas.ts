f01e737a3f4a9dbd5f2aa67ee332c8b3dbd9d916
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-04 12:37:46 -0300

    refactor: Update Despesas entity to use class syntax

--- src/domain/entities/despesas.ts
+++ src/domain/entities/despesas.ts
@@ -1,8 +1,10 @@
-export type Despesas = {
-  id?: string;
-  name: string;
-  description: string;
-  value: number;
-  date: Date;
-  deleted: boolean;
-};
+export class Despesa {
+  constructor(
+    public readonly id: string,
+    public readonly name: string,
+    public readonly description: string,
+    public readonly value: number,
+    public readonly date: Date,
+    public readonly deleted: boolean,
+  ) {}
+}
