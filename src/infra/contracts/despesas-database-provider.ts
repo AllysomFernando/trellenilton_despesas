431503b5108610e9da8cbfee7b0d0b4a387e5119
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-04 12:52:29 -0300

    refactor: Update database provider contracts to use consistent naming and imports

--- src/infra/contracts/despesas-database-provider.ts
+++ src/infra/contracts/despesas-database-provider.ts
@@ -1,9 +1,9 @@
-import { Despesa} from "@/domain/entities/despesa";
+import { Despesa } from '@/domain/entities/despesa';
 
 export interface IDespesasDatabaseProvider {
-	getDespesas(): Promise<Despesas[]>;
-	getDespesaById(id: string): Promise<Despesas>;
-	createDespesa(despesa: Despesas): Promise<Despesas>;
-	updateDespesa(id: string, despesa: Despesas): Promise<Despesas>;
-	deleteDespesa(id: string): Promise<void>;
+  getDespesas(): Promise<Despesa[]>;
+  getDespesaById(id: string): Promise<Despesa>;
+  createDespesa(despesa: Despesa): Promise<Despesa>;
+  updateDespesa(id: string, despesa: Despesa): Promise<Despesa>;
+  deleteDespesa(id: string): Promise<void>;
 }
--- src/infra/contracts/user-database-provider.ts
+++ src/infra/contracts/user-database-provider.ts
@@ -1,9 +1,9 @@
-import { User } from "@/domain/entities/user";
+import { User } from '@/domain/entities/user';
 
 export interface IUserDatabaseProvider {
-	getUsers(): Promise<User[]>;
-	getUserById(id: string): Promise<User>;
-	createUser(user: User): Promise<User>;
-	updateUser(id: string, user: User): Promise<User>;
-	deleteUser(id: string): Promise<User>;
+  getUsers(): Promise<User[]>;
+  getUserById(id: string): Promise<User>;
+  createUser(user: User): Promise<User>;
+  updateUser(id: string, user: User): Promise<User>;
+  deleteUser(id: string): Promise<User>;
 }
