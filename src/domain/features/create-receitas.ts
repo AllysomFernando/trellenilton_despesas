a52d106b517c70843ca53e68e572a0800a02f8fa
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-04 12:48:25 -0300

    refactor: Update create-receitas feature to use Receita entity and repository

--- src/domain/features/create-receitas.ts
+++ src/domain/features/create-receitas.ts
@@ -1,37 +1,37 @@
-import { Receitas } from "@/domain/entities/receitas";
-import { IReceitasRepository } from "@/domain/contracts/receitas-repository";
+import { Receita } from '@/domain/entities/receita';
+import { IReceitasRepository } from '@/domain/contracts/receita-repository';
 
 type Input = {
-	name: string;
-	description: string;
-	value: number;
-	date: Date;
+  name: string;
+  description: string;
+  value: number;
+  date: Date;
 };
 
-type OutPut = Receitas;
+type OutPut = Receita;
 type CreateReceitas = (input: Input) => Promise<OutPut>;
 type SetupCreateReceitas = {
-	repository: IReceitasRepository;
+  repository: IReceitasRepository;
 };
 type Setup = (props: SetupCreateReceitas) => CreateReceitas;
 
 export const SetupCreateReceitas: Setup =
-	({ repository }) =>
-	async ({ description, value, date, name }: Input) => {
-		if (!description) throw new Error("Descrição é obrigatória");
-		if (!value) throw new Error("value é obrigatório");
-		if (!name) throw new Error("Nome é obrigatório");
-		try {
-			return await repository.createReceita({
-				name,
-				description,
-				value,
-				date,
-				deleted: false,
-			});
-		} catch (error) {
-			throw new Error("Could not create receita: " + error, {
-				cause: "create-receitas",
-			});
-		}
-	};
+  ({ repository }) =>
+  async ({ description, value, date, name }: Input) => {
+    if (!description) throw new Error('Descrição é obrigatória');
+    if (!value) throw new Error('value é obrigatório');
+    if (!name) throw new Error('Nome é obrigatório');
+    try {
+      return await repository.createReceita({
+        name,
+        description,
+        value,
+        date,
+        deleted: false,
+      });
+    } catch (error) {
+      throw new Error('Could not create receita: ' + error, {
+        cause: 'create-receitas',
+      });
+    }
+  };
