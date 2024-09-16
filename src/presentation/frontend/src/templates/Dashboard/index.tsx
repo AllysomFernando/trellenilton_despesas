0e76d37feaf6592fbe3a07e96f8368daad888471
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-16 15:01:25 -0300

    feat: adicionar importação do React nos templates de Dashboard e Home

--- src/presentation/frontend/src/templates/Dashboard/index.tsx
+++ src/presentation/frontend/src/templates/Dashboard/index.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function DashboardTemplate() {
   return <div>Dashboard</div>;
 }
--- src/presentation/frontend/src/templates/Home/index.tsx
+++ src/presentation/frontend/src/templates/Home/index.tsx
@@ -1,12 +1,13 @@
-import {S} from './styles';
+import React from 'react';
+import { S } from './styles';
 export default function HomeTemplate() {
   return (
     <S.Container>
       <S.Title>Bem-vindo ao Controlador de despesas da Trellenilton</S.Title>
-          <S.Paragraph>
-            Aqui você pode controlar suas despesas de forma simples e eficiente.
-          </S.Paragraph>
-        <S.Paragraph>Experimente agora e aumente sua produtividade!</S.Paragraph>
-      </S.Container>
- );
+      <S.Paragraph>
+        Aqui você pode controlar suas despesas de forma simples e eficiente.
+      </S.Paragraph>
+      <S.Paragraph>Experimente agora e aumente sua produtividade!</S.Paragraph>
+    </S.Container>
+  );
 }
