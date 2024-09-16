a49b15d3a9fa2f89477c8bcb64e20bc8d70ca02b
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-16 13:37:24 -0300

    feat: Adicionar interface IReceita para modelagem de receitas

---
+++ src/presentation/frontend/src/types/models/IReceita.ts
@@ -0,0 +1,11 @@
+export interface IReceita {
+  id: number;
+  descricao: string;
+  valor: number;
+  data: string;
+  categoria: string;
+  tipo: string;
+  usuario_id: number;
+  created_at: string;
+  updated_at: string;
+}
