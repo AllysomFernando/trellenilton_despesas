7bbc328376b4f59a1952d42c22bc7648531efe89
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-16 12:50:50 -0300

    feat: Atualizar título e descrição do componente Home

--- src/presentation/frontend/src/templates/Home/index.tsx
+++ src/presentation/frontend/src/templates/Home/index.tsx
@@ -2,14 +2,11 @@ import {S} from './styles';
 export default function HomeTemplate() {
   return (
     <S.Container>
-      <S.Title>Bem-vindo ao ToDoList</S.Title>
-      <S.Paragraph>
-        Um ToDoList é uma aplicação que ajuda você a organizar suas tarefas
-        diárias. Você pode adicionar, editar e remover tarefas conforme
-        necessário, garantindo que você esteja sempre no controle de suas
-        atividades.
-      </S.Paragraph>
-      <S.Paragraph>Experimente agora e aumente sua produtividade!</S.Paragraph>
+      <S.Title>Bem-vindo ao Controlador de despesas da Trellenilton</S.Title>
+          <S.Paragraph>
+            Aqui você pode controlar suas despesas de forma simples e eficiente.
+          </S.Paragraph>
+        <S.Paragraph>Experimente agora e aumente sua produtividade!</S.Paragraph>
       </S.Container>
  );
 }
