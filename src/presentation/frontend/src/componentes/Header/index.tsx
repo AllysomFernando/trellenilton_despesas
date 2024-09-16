904f85d8a4c239047b65d3265c2a0bd4938ad8c1
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-16 14:55:16 -0300

    feat: adicionar link para a página de cadastro no cabeçalho

--- src/presentation/frontend/src/componentes/Header/index.tsx
+++ src/presentation/frontend/src/componentes/Header/index.tsx
@@ -15,6 +15,9 @@ const Header = () => {
           <li>
             <Link to="/login">Login</Link>
           </li>
+          <li>
+            <Link to="/cadastro">Cadastro</Link>
+          </li>
         </ul>
       </nav>
     </header>
