bce510833913e37450ff3345ed47bef29f10d22b
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-16 12:47:07 -0300

    feat: Adicionar estilos para o componente Home

--- src/presentation/frontend/src/templates/Home/styles.ts
+++ src/presentation/frontend/src/templates/Home/styles.ts
@@ -1,3 +1,28 @@
 import styled from 'styled-components';
 
-export const S = {};
+export const S = {
+    Container: styled.div`
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        gap: '1rem',
+    `,
+    Title : styled.h1`
+        color: #333;
+        margin-bottom: 20px;
+        align-self: center;
+        text-align: center;
+    `,
+    Paragraph : styled.p`
+        color: #666;
+        font-size: 1.2em;
+        line-height: 1.5;
+        max-width: 600px;
+        text-align: center;
+        margin-bottom: 20px;
+        align-self: center;
+        text-align: center;
+    `
+};
