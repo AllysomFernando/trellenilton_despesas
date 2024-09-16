4e61c4791e6661eab73959f20c7c665a4cee0f83
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-16 15:06:59 -0300

    feat: adicionar campo 'name' e tornar propriedades opcionais em IDespesa

--- src/presentation/frontend/src/types/models/IDespesas.ts
+++ src/presentation/frontend/src/types/models/IDespesas.ts
@@ -1,11 +1,12 @@
 export interface IDespesa {
   id: number;
+  name: string;
   descricao: string;
   valor: number;
   data: string;
   categoria: string;
   tipo: string;
-  usuario_id: number;
-  created_at: string;
-  updated_at: string;
+  usuario_id?: number;
+  created_at?: string;
+  updated_at?: string;
 }
