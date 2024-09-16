41e94903e5766f2f9b7945174eef7d8f4b14988b
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-16 16:03:55 -0300

    feat: atualizar interface IDespesa para usar ICategoria em vez de string

--- src/presentation/frontend/src/types/models/IDespesas.ts
+++ src/presentation/frontend/src/types/models/IDespesas.ts
@@ -1,11 +1,12 @@
+import { ICategoria } from './ITipos';
+
 export interface IDespesa {
   id: string;
   name: string;
   descricao: string;
   valor: number;
   data: string;
-  categoria: string;
-  tipo: string;
+  categoria: ICategoria;
   usuario_id?: number;
   created_at?: string;
   updated_at?: string;
