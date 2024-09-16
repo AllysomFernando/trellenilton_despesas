49054db7864562d82a2defd3ebfa296447a23db2
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-16 12:47:02 -0300

    feat: Atualizar o template da página inicial com novos estilos

--- src/presentation/frontend/src/templates/Home/index.tsx
+++ src/presentation/frontend/src/templates/Home/index.tsx
@@ -1,14 +1,15 @@
+import {S} from './styles';
 export default function HomeTemplate() {
   return (
-    <div>
-      <h1>Bem-vindo ao ToDoList</h1>
-      <p>
+    <S.Container>
+      <S.Title>Bem-vindo ao ToDoList</S.Title>
+      <S.Paragraph>
         Um ToDoList é uma aplicação que ajuda você a organizar suas tarefas
         diárias. Você pode adicionar, editar e remover tarefas conforme
         necessário, garantindo que você esteja sempre no controle de suas
         atividades.
-      </p>
-      <p>Experimente agora e aumente sua produtividade!</p>
-    </div>
-  );
+      </S.Paragraph>
+      <S.Paragraph>Experimente agora e aumente sua produtividade!</S.Paragraph>
+      </S.Container>
+ );
 }
