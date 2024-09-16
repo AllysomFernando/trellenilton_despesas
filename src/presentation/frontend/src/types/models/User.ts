739ad16cec461fec2f393f149d5297ae63724a9b
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-16 14:52:14 -0300

    feat: tornar propriedades id, created_at e updated_at opcionais no modelo User

--- src/presentation/frontend/src/types/models/User.ts
+++ src/presentation/frontend/src/types/models/User.ts
@@ -1,8 +1,8 @@
 export interface User {
-  id: number;
+  id?: number;
   name: string;
   email: string;
   password: string;
-  created_at: string;
-  updated_at: string;
+  created_at?: string;
+  updated_at?: string;
 }
