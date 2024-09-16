1a4491ab07efac6887453dbd7f3f0cfbe97f25f8
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-16 11:55:54 -0300

    feat: Adicionar link de Login ao componente Header

--- src/presentation/frontend/src/componentes/Header/index.tsx
+++ src/presentation/frontend/src/componentes/Header/index.tsx
@@ -6,8 +6,15 @@ const Header = () => {
     <header>
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/sobre">Sobre</Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/sobre">Sobre</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
         </ul>
       </nav>
     </header>
