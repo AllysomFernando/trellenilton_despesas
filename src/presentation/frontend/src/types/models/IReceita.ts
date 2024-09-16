ca4c8424c1c69a4793d67a55fdc8dcf1085e0e9f
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-16 16:05:22 -0300

    feat: atualizar interface IReceita para usar ICategoria em vez de string

--- src/presentation/frontend/src/types/models/IReceita.ts
+++ src/presentation/frontend/src/types/models/IReceita.ts
@@ -1,10 +1,12 @@
+import { ICategoria } from './ITipos';
+
 export interface IReceita {
   id: string;
   name: string;
   descricao: string;
   valor: number;
   data: string;
-  categoria: string;
+  categoria: ICategoria;
   usuario_id?: number;
   created_at?: string;
   updated_at?: string;
