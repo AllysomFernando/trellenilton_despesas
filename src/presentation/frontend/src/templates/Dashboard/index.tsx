23b34a84bcb700fcedfce6fc6c1c60b43b2273a7
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-16 15:06:56 -0300

    feat: adicionar exibição de despesas e receitas no template do Dashboard

--- src/presentation/frontend/src/templates/Dashboard/index.tsx
+++ src/presentation/frontend/src/templates/Dashboard/index.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 export default function DashboardTemplate() {
-  return <div>Dashboard</div>;
+  const { despesas, receitas, user } = useContext(AuthContext);
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <p>Seja bem-vindo ao seu dashboard {user.name} </p>
+
+      <h3>Despesas</h3>
+      <ul>
+        {despesas.map((despesa) => (
+          <li key={despesa.id}>{despesa.name}</li>
+        ))}
+      </ul>
+
+      <h3>Receitas</h3>
+      <ul>
+        {receitas.map((receita) => (
+          <li key={receita.id}>{receita.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
