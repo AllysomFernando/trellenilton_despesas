b2b8e0f14b0dc3c9f6b68244b4a77454e3d05d77
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-17 09:08:27 -0300

    feat: alterar direção do layout da lista no componente Dashboard

--- src/presentation/frontend/src/templates/Dashboard/styles.ts
+++ src/presentation/frontend/src/templates/Dashboard/styles.ts
@@ -41,7 +41,7 @@ export const S = {
     width: 100%;
     ul {
       display: flex;
-      flex-direction: row;
+      flex-direction: column;
       align-items: center;
       justify-content: center;
     }
