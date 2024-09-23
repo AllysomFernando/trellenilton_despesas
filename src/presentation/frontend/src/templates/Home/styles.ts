ef60323cb478c1d6ea862dac60117806df53830b
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-23 11:04:02 -0300

    refactor: ajustar formatação e indentação em diversos arquivos

--- src/presentation/frontend/src/templates/Home/styles.ts
+++ src/presentation/frontend/src/templates/Home/styles.ts
@@ -1,31 +1,31 @@
 import styled from 'styled-components';
 
 export const S = {
-    Container: styled.div`
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: 600px;
-        height: '100vh',
-        gap: '1rem',
-    `,
-    Title : styled.h1`
-        color: #333;
-        margin-bottom: 20px;
-        align-self: center;
-        text-align: center;
-    `,
-    Paragraph : styled.p`
-        color: #666;
-        font-size: 1.2em;
-        line-height: 1.5;
-        margin-bottom: 20px;
-        text-align: center;
-        align-self: center;
-        align-items: center;
-        margin: 0 auto;
-        justify-content: center;
-        width: 50%;
-    `
+  Container: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 600px;
+    height: 100vh;
+    gap: 1rem;
+  `,
+  Title: styled.h1`
+    color: #333;
+    margin-bottom: 20px;
+    align-self: center;
+    text-align: center;
+  `,
+  Paragraph: styled.p`
+    color: #666;
+    font-size: 1.2em;
+    line-height: 1.5;
+    margin-bottom: 20px;
+    text-align: center;
+    align-self: center;
+    align-items: center;
+    margin: 0 auto;
+    justify-content: center;
+    width: 50%;
+  `,
 };
